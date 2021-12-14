cab899f2325bf716cdeed2c7ef9e3e142fddfdef
Robert <RobertWSeifert@gmail.com>
2021-12-14 08:24:27 -0700

    Added a .NET 6 version
    * Use Microsoft.Extensions.DependencyInjection
    * Use Toolkit.MVVM

---
+++ src/KeepAwakeTray6/App.xaml.cs
@@ -0,0 +1,62 @@
+﻿using Hardcodet.Wpf.TaskbarNotification;
+using KeepAwakeTray6.Interfaces;
+using KeepAwakeTray6.Managers;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Reflection;
+using System.Threading;
+using System.Windows;
+
+namespace KeepAwakeTray6
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private IServiceProvider serviceProvider;
+
+        private Mutex singleInstanceMutex;
+        private TaskbarIcon notifyIcon;
+        private IAppManager appManager;
+
+        public App()
+        {
+            //
+            // Only allow one instance of the app to run
+            //
+            var s = Assembly.GetExecutingAssembly().GetType().GUID.ToString();
+            singleInstanceMutex = new Mutex(true, s, out var isNew);
+
+            if (!isNew)
+                Environment.Exit(0);
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            IServiceCollection services = new ServiceCollection();
+            ConfigureServices(services);
+
+            notifyIcon = (TaskbarIcon)FindResource("TheNotifyIcon");
+
+            serviceProvider = services.BuildServiceProvider();
+            appManager = serviceProvider.GetService<IAppManager>();
+
+            if (notifyIcon != null)
+                notifyIcon.DataContext = appManager;
+        }
+
+        private void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<IAppManager, AppManager>();
+            services.AddSingleton<IActivityMonitor, ActivityMonitor>();
+            services.AddSingleton<ISettingsManager, SettingsManager>();
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            notifyIcon.Dispose();
+            base.OnExit(e);
+        }
+    }
+}
---
+++ src/KeepAwakeTray6/Views/SettingsView.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Text.RegularExpressions;
+using KeepAwakeTray6.ViewModels;
+using System.Windows;
+using System.Windows.Input;
+
+namespace KeepAwakeTray6.Views
+{
+    /// <summary>
+    /// Interaction logic for SettingsView.xaml
+    /// </summary>
+    public partial class SettingsView : Window
+    {
+        private readonly Regex allowdCharachters = new Regex("[^0-9]+");
+
+        public SettingsView(SettingsViewModel vm)
+        {
+            InitializeComponent();
+
+            DataContext = vm;
+            vm.CloseRequested += (sender, args) => Close();
+        }
+
+        private void MoveWindow(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+        private void UIElement_OnPreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = allowdCharachters.IsMatch(e.Text);
+        }
+    }
+}
